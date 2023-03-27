e6f8914652dadd6943f3e38a52b81038a6b0ac98
Kamil Smoczynski <kamilsmoczynski9309@gmail.com>
2023-03-27 11:58:49 +0200

    adding a function to the button to send up the form

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const userName = document.querySelector('#username')
+
+
+const username = document.querySelector('#username')
 const pass = document.querySelector('#password')
 const pass2 = document.querySelector('#password2')
 const email = document.querySelector('#email')
@@ -7,10 +9,43 @@ const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
 
+const showError = (input, msg) => {
+    const formBox = input.parentElement
+    const erroeMsg = formBox.querySelector('.error-text')
+
+    formBox.classList.add('error')
+    erroeMsg.textContent = msg
+}
+
+const clearError = input => {
+    const formBox = input.parentElement
+    formBox.classList.remove('error')
+
+}
+
+
+const checkForm = input => {
+    input.forEach(el => {
+        if(el.value === '') {
+            showError(el, el.placeholder)
+        }else {
+            clearError(el)
+        }
+    })
+}
+
+  
+sendBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+
+    checkForm([username, pass, pass2, email]);
+})
+
 clearBtn.addEventListener('click', e => {
     e.preventDefault();
 
-    [userName, pass, pass2, email].forEach(el => {
+    [username, pass, pass2, email].forEach(el => {
         el.value= ''
     })
 
